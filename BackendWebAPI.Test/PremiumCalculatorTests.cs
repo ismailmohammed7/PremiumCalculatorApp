08d3aa9fbe335dbfd97a7052cc1fec3f0913f6b1
ismailmohammed7 <42437777+ismailmohammed7@users.noreply.github.com>
2022-10-10 13:54:19 +0530

    Update PremiumCalculatorTests.cs

--- BackendWebAPI.Test/PremiumCalculatorTests.cs
+++ BackendWebAPI.Test/PremiumCalculatorTests.cs
@@ -18,7 +18,7 @@ namespace BackendWebAPI.Test
             double sum = 20000;
             string occupation = "Doctor";
             int age = 25;
-            double premium = 416.667;
+            double premium = 41.667;
 
             _serviceMock.Setup<double>(x => x.CalculatePremium(It.IsAny<PremiumDetail>())).Returns(premium);
             PremiumCalculatorController premiumCalculator = new PremiumCalculatorController(Mock.Of<Microsoft.Extensions.Logging.ILogger<PremiumCalculatorController>>(), _serviceMock.Object);
@@ -30,7 +30,7 @@ namespace BackendWebAPI.Test
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var resultValue = ((OkObjectResult)result).Value;
             Assert.IsNotNull(resultValue);
-            Assert.AreEqual(resultValue, 416.667);
+            Assert.AreEqual(resultValue, 41.667);
         }
     }
 }
