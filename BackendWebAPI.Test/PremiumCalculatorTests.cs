76501bb7ce333cc23587cab3b3cd3c3316ca64de
Ismail <Ismail@DESKTOP-TPIITTR>
2022-10-10 16:55:03 +0530

    Add More Unit tests

--- BackendWebAPI.Test/PremiumCalculatorTests.cs
+++ BackendWebAPI.Test/PremiumCalculatorTests.cs
@@ -29,8 +29,30 @@ namespace BackendWebAPI.Test
             //Assert
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var resultValue = ((OkObjectResult)result).Value;
-            Assert.IsNotNull(resultValue);
-            Assert.AreEqual(resultValue, 41.667);
+            var resultStatusCode = (int?)((OkObjectResult)result).StatusCode;
+            Assert.AreEqual(200, resultStatusCode);
+            Assert.AreEqual(41.667, resultValue);
+        }
+
+        [TestMethod]
+        public void PremiumController_GetPremiumBadRequest()
+        {
+            //Arrange
+            double sum = 20000;
+            string occupation = "Doctor";
+            int age = 25;
+            double premium = -1;
+
+            _serviceMock.Setup<double>(x => x.CalculatePremium(It.IsAny<PremiumDetail>())).Returns(premium);
+            PremiumCalculatorController premiumCalculator = new PremiumCalculatorController(Mock.Of<Microsoft.Extensions.Logging.ILogger<PremiumCalculatorController>>(), _serviceMock.Object);
+
+            //Act
+            var result = premiumCalculator.CalculatePremium(age, sum, occupation).Result;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            var resultStatusCode = (int?)((BadRequestResult)result).StatusCode;
+            Assert.AreEqual(400, resultStatusCode);
         }
     }
 }
---
+++ BackendWebAPI.Test/PremiumServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using BackendWebAPI.Models;
+using BackendWebAPI.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendWebAPI.Test
+{
+
+    [TestClass]
+    public class PremiumServiceTests
+    {
+        [TestMethod]
+        public void CalculatePremium_ReturnNonZeroValue()
+        {
+            //Arrange
+            PremiumDetail premiumDetail = new PremiumDetail
+            {
+                Age = 25,
+                SumInsured = 20000,
+                Occupation = "Doctor"
+            };
+            PremiumService premiumService = new PremiumService();
+
+            //Act
+            double premium = premiumService.CalculatePremium(premiumDetail);
+
+            //Assert
+            Assert.AreEqual(41.667, premium);
+        }
+
+        [TestMethod]
+        public void CalculatePremium_ReturnNegativeValueInCaseofBadRequest()
+        {
+            //Arrange
+            PremiumDetail premiumDetail = new PremiumDetail
+            {
+                Age = -25,
+                SumInsured = 20000,
+                Occupation = "Doctor"
+            };
+            PremiumService premiumService = new PremiumService();
+
+            //Act
+            double premium = premiumService.CalculatePremium(premiumDetail);
+
+            //Assert
+            Assert.AreEqual(-41.667, premium);
+        }
+
+    }
+}
